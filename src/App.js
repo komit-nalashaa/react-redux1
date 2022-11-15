213895607ef10c9007aea78d05bef201305dc112
komit-nalashaa <komit@nalashaa.net>
2022-11-15 19:43:37 +0530

    addition and subtraction value complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 function App() {
   const counter = useSelector((state)=> state.counter)
+  const dispatch = useDispatch();
+
+  const increment=() => {
+    dispatch({ type: 'INC'});
+  }
+
+  const decrement=() => {
+    dispatch({ type: 'DSC'});
+  }
+
+  const addVal=() => {
+    dispatch({type: 'ADDVAL', payload:28});
+  }
   return (
     <div>
       <h1>Counter App  </h1>
       <h2> {counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addVal}>Add 10 Value</button>
+
+
       
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,21 @@ import { legacy_createStore as createStore } from "redux";
 
 
 const reducerFn = (state= {counter: 0}, action) => {
+
+    if(action.type === 'INC') {
+        return {counter: state.counter + 1}
+    }
+
+    if(action.type === 'DSC') {
+        return {counter: state.counter - 1}
+    }
+
+    if(action.type === "ADDVAL"){
+        return {counter: state.counter + action.payload }
+    }
+
+
+
     return state ; 
 
 };
